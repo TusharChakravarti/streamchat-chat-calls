053bb03acf85ac80659350ffe0f01299a08fd976
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-04-10 15:10:19 +0530

    initial commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,24 +26,7 @@ const Navbar = () => {
           {isChatPage && (
             <div className="pl-5">
               <Link to="/" className="flex items-center gap-2.5" >
-               
-   {/* <div className="mb-4 flex items-center justify-start gap-2" data-theme={theme}>
-  <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-    <svg width={36} height={36} viewBox="0 0 36 36" fill="none">
-      <rect x="6" y="8" width="16" height="11" rx="4" className="fill-primary opacity-40" />
-      <polygon points="8,19 6,25 14,19" className="fill-primary opacity-40" />
-      <rect x="14" y="17" width="16" height="11" rx="4" className="fill-primary" />
-      <polygon points="28,28 30,34 22,28" className="fill-primary" />
-      
-    </svg>
-
-    <span style={{ fontSize: "26px", fontWeight: "700", letterSpacing: "-0.4px", lineHeight: 1 }}>
-      <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">Stream</span>
-      <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">Chat</span>
-    </span>
-  </div>
-</div> */}
-
+ 
 
 
 
