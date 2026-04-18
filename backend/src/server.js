8d0babc879ba28c279bc8747a4bd10b34767c3f7
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-04-18 20:18:32 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,6 +26,10 @@ app.use('/api/auth',authRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/chat', chatRoutes) 
 
+app.get("/", (req, res) => {
+  res.json({ message: "Backend working" });
+});
+
 // if(process.env.NODE_ENV === "production"){
 //     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 //     app.get("*",(req,res) => res.sendFile(path.join(__dirname,"../frontend","dist","index.html")))
