583c2208b6e8fe0c9d7093865759471ee04ecdd9
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-04-18 21:49:10 +0530

    StreamChat

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -86,7 +86,7 @@ if(!isPasswordCorrect) return res.status(401).json({message:"Invalid email or pa
    res.cookie("jwt",token,{
     maxAge:7*24*60*60*1000, // 7 days
     httpOnly:true, // prevent XSS attacks,
-    sameSite:"strict",//prevent CSRF attacks,
+    sameSite:"None",//prevent CSRF attacks,
     secure:process.env.NODE_ENV === 'production' // only send cookie over HTTPS in production
    })
    res.status(200).json({success:true,user})
