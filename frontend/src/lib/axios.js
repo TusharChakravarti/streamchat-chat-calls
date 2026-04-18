a9e6fb65e18e68f330f6cca281134d787a725bff
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-04-18 23:40:43 +0530

    StreamChat

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
- const BASE_URL  = import.meta.env.MODE === "development"?"http://localhost:5001/api": import.meta.env.VITE_API_URL;
-
-
+const BASE_URL  = import.meta.env.MODE === "development"?"http://localhost:5001/api":"https://your-backend.onrender.com/api"
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials:true, // send cookies with the request
