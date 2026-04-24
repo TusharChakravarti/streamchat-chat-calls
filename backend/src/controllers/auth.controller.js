f4ef98fadd05c178f101559279dee6393f1164b7
Tushar Chakravarti <tusharch040920052gmail.com>
2026-04-24 10:11:10 +0530

    StreamChat

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -10,7 +10,7 @@ const issueToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: 'None',
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
   })
   return token
 }
