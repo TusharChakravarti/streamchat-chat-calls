ffaf224f1ffb61896097dc3df3f6e7d25efeb27d
Tushar Chakravarti <tusharch040920052gmail.com>
2026-04-26 18:53:39 +0530

    StreamChat

--- backend/src/lib/multer.js
+++ backend/src/lib/multer.js
@@ -6,7 +6,7 @@ export const upload = multer({
     storage,
     limits:{fileSize:5*1024*1024}, // Max 5MB
     fileFilter:(req,file,cb)=>{
-        if(file.mimetype.startsWith('/image')){
+        if(file.mimetype.startsWith('image/')){
             cb(null,true);
         }
         else{
